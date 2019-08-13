f97f6c6d77f99999ad1490bcd234276f93f6048a
Fernando Guerra <fernandoguerra@Fernandos-MacBook-Pro.local>
2019-08-13 15:59:16 +0200

    Initial commit

--- src/main/java/RomanConverter.java
+++ src/main/java/RomanConverter.java
@@ -1,2 +1,88 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanConverter {
+
+
+    private Map<Integer, String> romanByArabicNumbers = new HashMap<Integer, String>();
+
+    public RomanConverter() {
+        romanByArabicNumbers.put(1, "I");
+        romanByArabicNumbers.put(4, "IV");
+        romanByArabicNumbers.put(5, "V");
+        romanByArabicNumbers.put(9, "IX");
+        romanByArabicNumbers.put(10, "X");
+    }
+
+    public String convert(int arabic) {
+        StringBuffer result = new StringBuffer("");
+
+        while (1000 <= arabic) {
+            result.append("M");
+            arabic -= 1000;
+        }
+
+        if (arabic >= 900 && arabic < 1000) {
+            result.append("CM");
+            arabic -= 900;
+        }
+
+        if (arabic >= 500) {
+            result.append("D");
+            arabic -= 500;
+        }
+
+        if (arabic >= 400 && arabic < 500) {
+            result.append("CD");
+            arabic -= 400;
+        }
+
+        while (100 <= arabic) {
+            result.append("C");
+            arabic -= 100;
+        }
+
+        if (arabic >= 90 && arabic < 100) {
+            result.append("XC");
+            arabic -= 90;
+        }
+
+        if (arabic >= 50) {
+            result.append("L");
+            arabic -= 50;
+        }
+
+        if (arabic >= 40 && arabic < 50) {
+            result.append("XL");
+            arabic -= 40;
+        }
+
+        while (10 <= arabic) {
+            arabic = addExactNumber(arabic, result, 10);
+        }
+
+        arabic = addExactNumber(arabic, result, 9);
+
+        arabic = addExactNumber(arabic, result, 5);
+
+        arabic = addExactNumber(arabic, result, 4);
+
+        while (1 <= arabic) {
+            arabic = addExactNumber(arabic, result, 1);
+        }
+
+        return result.toString();
+    }
+
+    private int addExactNumber(int arabic, StringBuffer result, int exactNumber) {
+        String romanNumber = romanByArabicNumbers.get(exactNumber);
+        if (arabic >= exactNumber) {
+            result.append(romanNumber);
+            arabic -= exactNumber;
+        }
+        return arabic;
+    }
+
+
 }
--- src/test/java/RomanConverterShould.java
+++ src/test/java/RomanConverterShould.java
@@ -1,2 +1,47 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
 public class RomanConverterShould {
+
+    @Test
+    @Parameters({
+            "1, I",
+            "2, II",
+            "5, V",
+            "6, VI",
+            "7, VII",
+            "10, X",
+            "13, XIII",
+            "15, XV",
+            "16, XVI",
+            "20, XX",
+            "21, XXI",
+            "25, XXV",
+            "51, LI",
+            "55, LV",
+            "65, LXV",
+            "88, LXXXVIII",
+            "188, CLXXXVIII",
+            "888, DCCCLXXXVIII",
+            "3888, MMMDCCCLXXXVIII",
+
+            "4, IV",
+            "9, IX",
+            "14, XIV",
+            "34, XXXIV",
+            "40, XL",
+            "43, XLIII",
+            "93, XCIII",
+            "493, CDXCIII",
+            "993, CMXCIII",
+    })
+    public void convert(int arabic, String roman) {
+        RomanConverter romanConverter = new RomanConverter();
+        assertEquals(roman, romanConverter.convert(arabic));
+    }
 }
